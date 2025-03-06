7e1f366303940d70fc0401b2aabfc48f72e45816
小久保尚 <kokubonao@kokubonaonoMacBook-Air.local>
2025-03-06 15:16:48 +0900

    front mvp

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Image from "next/image";
 import Heatmap from "@/components/Heatmap";
 import PredictionChart from "@/components/PredictionChart";
 
